6f236e6978b7346afe95067ee1c9985e6b9acc4d
Uahmed2031 <uahmed2031@gmail.com>
2024-12-18 01:01:29 -0500

    final push

--- src/test/java/com/TN/Runner/Runner_TestNG.java
+++ src/test/java/com/TN/Runner/Runner_TestNG.java
@@ -9,10 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 		glue = {"com.TN.Step_Definitions", "hooks"},
 		tags = "@TutorialsNinjaLogin or @TutorialsNinjaRegister or @TutorialsNinjaSearchProduct or @TutorialsNinjaAddToCart or @TutorialsNinjaLogout",
 		monochrome = true,
-		//plugin = {"html:test-output\\ExtentReportBDD.html"}
-		
-		plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-
+		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		)
 
 public class Runner_TestNG extends AbstractTestNGCucumberTests{
